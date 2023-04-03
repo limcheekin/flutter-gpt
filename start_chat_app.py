a31fe1df091b337301a8176382c411c8ce23ad0c
Lim Chee Kin <limcheekin@vobject.com>
2023-04-03 18:08:15 +0800

    feat: updated code for chat app, chain_type='stuff' seems doesn't work well with chat_history

--- start_chat_app.py
+++ start_chat_app.py
@@ -1,17 +1,11 @@
 import pickle
-from langchain.chains import VectorDBQAWithSourcesChain
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts.chat import (
     ChatPromptTemplate,
     SystemMessagePromptTemplate,
-    AIMessagePromptTemplate,
     HumanMessagePromptTemplate,
 )
-from langchain.schema import (
-    AIMessage,
-    HumanMessage,
-    SystemMessage
-)
+from langchain.chains import RetrievalQAWithSourcesChain
 
 system_template="""Use the following pieces of context to answer the users question. 
 If you don't know the answer, just say "Hmm..., I'm not sure.", don't try to make up an answer.
@@ -23,25 +17,28 @@ Example of your response should be:
 ```
 The answer is foo
 
-Sources: xyz
+Sources:
+1. abc
+2. xyz
 ```
-
 Begin!
 ----------------
-{summaries}"""
+{summaries}
+"""
 messages = [
     SystemMessagePromptTemplate.from_template(system_template),
     HumanMessagePromptTemplate.from_template("{question}")
 ]
 prompt = ChatPromptTemplate.from_messages(messages)
 
-def get_chain(vectorstore, prompt):
+def get_chain(store):
     chain_type_kwargs = {"prompt": prompt}
-    chain = VectorDBQAWithSourcesChain.from_chain_type(
-    ChatOpenAI(temperature=0), 
-        chain_type="stuff", 
-        vectorstore=vectorstore,
-        chain_type_kwargs=chain_type_kwargs
+    chain = RetrievalQAWithSourcesChain.from_chain_type(
+        llm=ChatOpenAI(temperature=0),
+        chain_type="stuff",
+        retriever=store.as_retriever(),
+        chain_type_kwargs=chain_type_kwargs,
+        reduce_k_below_max_tokens=True
     )
     return chain
 
@@ -49,12 +46,12 @@ def get_chain(vectorstore, prompt):
 if __name__ == "__main__":
     with open("faiss_store.pkl", "rb") as f:
         vectorstore = pickle.load(f)
-    qa_chain = get_chain(vectorstore, prompt)
+    qa_chain = get_chain(vectorstore)
+    chat_history = []
     print("Chat with the FlutterGPT bot:")
     while True:
         print("Your question:")
         question = input()
-        result = qa_chain({"question": question}, return_only_outputs=True)
+        result = qa_chain({"question": question, "chat_history": chat_history})
         print(f"Answer: {result['answer']}")
-        print("\n\n")
-        print(result)
+        chat_history.append((question, result["answer"]))
