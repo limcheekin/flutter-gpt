8cb7f355f07aabe0628f336f4f5b568dbc1d3810
Lim Chee Kin <limcheekin@vobject.com>
2023-04-04 22:00:42 +0800

    feat: updated to hugging face classes

--- ask_question.py
+++ ask_question.py
@@ -1,10 +1,63 @@
 import argparse
-import faiss
-import os
 import pickle
 
-from langchain.llms import HuggingFaceHub
-from langchain.chains import VectorDBQAWithSourcesChain
+from langchain.llms import HuggingFacePipeline
+from transformers import GPT2Tokenizer, GPT2Model, pipeline, set_seed
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.chains import RetrievalQAWithSourcesChain
+
+system_template = """Use the following pieces of context to answer the users question. 
+If you don't know the answer, just say "Hmm..., I'm not sure.", don't try to make up an answer.
+ALWAYS return a "Sources" part in your answer.
+The "Sources" part should be a reference to the source of the document from which you got your answer.
+Example of your response should be:
+```
+The answer is foo
+Sources:
+1. abc
+2. xyz
+```
+Begin!
+----------------
+{summaries}
+"""
+messages = [
+    SystemMessagePromptTemplate.from_template(system_template),
+    HumanMessagePromptTemplate.from_template("{question}")
+]
+prompt = ChatPromptTemplate.from_messages(messages)
+
+
+def get_llm():
+    model_id = "gpt2"
+    # tokenizer = GPT2Tokenizer.from_pretrained(model_id)
+    # model = GPT2Model.from_pretrained(model_id)
+
+    pipe = pipeline(
+        "text-generation",
+        model=model_id,
+        max_length=1024
+    )
+    set_seed(55)
+    llm = HuggingFacePipeline(pipeline=pipe)
+    return llm
+
+
+def get_chain(store):
+    chain_type_kwargs = {"prompt": prompt}
+    chain = RetrievalQAWithSourcesChain.from_chain_type(
+        get_llm(),
+        chain_type="stuff",
+        retriever=store.as_retriever(),
+        chain_type_kwargs=chain_type_kwargs,
+        reduce_k_below_max_tokens=True
+    )
+    return chain
+
 
 parser = argparse.ArgumentParser(description='FlutterGPT Q&A')
 parser.add_argument('question', type=str, help='Your question for FlutterGPT')
@@ -12,10 +65,8 @@ args = parser.parse_args()
 
 with open("faiss_store.pkl", "rb") as f:
     store = pickle.load(f)
-llm = HuggingFaceHub(repo_id="google/flan-t5-large", model_kwargs={"temperature":0})
-chain = VectorDBQAWithSourcesChain.from_llm(llm=llm, vectorstore=store, verbose=True)
+chain = get_chain(store)
 result = chain({"question": args.question})
 
 print(f"Answer: {result['answer']}")
 print(f"Sources: {result['sources']}")
-
--- create_embeddings.py
+++ create_embeddings.py
@@ -1,16 +1,20 @@
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import UnstructuredHTMLLoader
 from langchain.vectorstores.faiss import FAISS
-from langchain.embeddings import HuggingFaceHubEmbeddings
+from langchain.embeddings import HuggingFaceEmbeddings
+from transformers import GPT2TokenizerFast
 import pickle
+
 
 def ingest_data():
     file_paths = None
-    with open('html_files_index.txt', 'r') as file: 
+    with open('html_files_index.txt', 'r') as file:
         file_paths = file.readlines()
 
     docs = []
-    text_splitter = RecursiveCharacterTextSplitter()
+    tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
+    text_splitter = RecursiveCharacterTextSplitter.from_huggingface_tokenizer(
+        tokenizer, chunk_size=512, chunk_overlap=0)
 
     print("Load HTML files locally...")
     for i, file_path in enumerate(file_paths):
@@ -21,12 +25,14 @@ def ingest_data():
         print(f"{i+1})Split {file_path} into {len(splits)} chunks")
 
     print("Load data to FAISS store")
-    store = FAISS.from_documents(docs, HuggingFaceHubEmbeddings())
+    model_name = "sentence-transformers/all-mpnet-base-v2"
+    store = FAISS.from_documents(
+        docs, HuggingFaceEmbeddings(model_name=model_name))
 
     print("Save faiss_store.pkl")
     with open("faiss_store.pkl", "wb") as f:
         pickle.dump(store, f)
-        
+
+
 if __name__ == "__main__":
     ingest_data()
-
