93d840f67d1cb810ff766a6d40c6cdf39d35325a
Lim Chee Kin <limcheekin@vobject.com>
2023-04-12 15:14:33 +0800

    feat: updated ask question code to use flan-t5-base model

--- ask_question.py
+++ ask_question.py
@@ -13,23 +13,13 @@ from langchain.chains import RetrievalQAWithSourcesChain
 print('loading model...')
 
 try:
-    model = AutoModelForSeq2SeqLM.from_pretrained("flan-t5-small/")
-    tokenizer = AutoTokenizer.from_pretrained("flan-t5-small/")
+    model = AutoModelForSeq2SeqLM.from_pretrained("flan-t5-base/")
+    tokenizer = AutoTokenizer.from_pretrained("flan-t5-base/")
 except:
     print("An exception occurred on loading model.")
 
 system_template = """Use the following pieces of context to answer the users question. 
 If you don't know the answer, just say "Hmm..., I'm not sure.", don't try to make up an answer.
-ALWAYS return a "Sources" part in your answer.
-The "Sources" part should be a reference to the source of the document from which you got your answer.
-Example of your response should be:
-```
-The answer is foo
-Sources:
-1. abc
-2. xyz
-```
-Begin!
 ----------------
 {summaries}
 """
@@ -45,9 +35,15 @@ def get_llm():
         "text2text-generation",
         model=model,
         tokenizer=tokenizer,
+        min_length=128,
         max_length=256,
         temperature=0.0,
         use_fast=True,
+        length_penalty=2,
+        num_beams=4,
+        no_repeat_ngram_size=2,
+        num_return_sequences=1,
+        early_stopping=True,
     )
     set_seed(55)
     llm = HuggingFacePipeline(pipeline=pipe)
