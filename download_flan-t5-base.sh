a915316d75a0881a83f087881fe52171e388c6ee
Lim Chee Kin <limcheekin@vobject.com>
2023-04-17 13:07:40 +0800

    chore: added script for download flan-t5-large model and update other flan-t5 models download script

--- download_flan-t5-base.sh
+++ download_flan-t5-base.sh
@@ -1,9 +1,9 @@
-mkdir flan-t5-base
-curl -L https://huggingface.co/google/flan-t5-base/resolve/main/pytorch_model.bin -o ./flan-t5-base/pytorch_model.bin
-curl https://huggingface.co/google/flan-t5-base/raw/main/config.json -o ./flan-t5-base/config.json
-curl https://huggingface.co/google/flan-t5-base/raw/main/generation_config.json -o ./flan-t5-base/generation_config.json
+mkdir -p google/flan-t5-base
+curl -L https://huggingface.co/google/flan-t5-base/resolve/main/pytorch_model.bin -o ./google/flan-t5-base/pytorch_model.bin
+curl https://huggingface.co/google/flan-t5-base/raw/main/config.json -o ./google/flan-t5-base/config.json
+curl https://huggingface.co/google/flan-t5-base/raw/main/generation_config.json -o ./google/flan-t5-base/generation_config.json
 
-curl https://huggingface.co/google/flan-t5-base/raw/main/tokenizer.json -o ./flan-t5-base/tokenizer.json
-curl https://huggingface.co/google/flan-t5-base/raw/main/tokenizer_config.json -o ./flan-t5-base/tokenizer_config.json
-curl https://huggingface.co/google/flan-t5-base/raw/main/special_tokens_map.json -o ./flan-t5-base/special_tokens_map.json
-curl https://huggingface.co/google/flan-t5-base/resolve/main/spiece.model -o ./flan-t5-base/spiece.model
+curl https://huggingface.co/google/flan-t5-base/raw/main/tokenizer.json -o ./google/flan-t5-base/tokenizer.json
+curl https://huggingface.co/google/flan-t5-base/raw/main/tokenizer_config.json -o ./google/flan-t5-base/tokenizer_config.json
+curl https://huggingface.co/google/flan-t5-base/raw/main/special_tokens_map.json -o ./google/flan-t5-base/special_tokens_map.json
+curl https://huggingface.co/google/flan-t5-base/resolve/main/spiece.model -o ./google/flan-t5-base/spiece.model
