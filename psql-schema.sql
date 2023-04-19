fff2ca3380401eec7e3f45150f983685b9540344
Lim Chee Kin <limcheekin@vobject.com>
2023-04-19 17:41:00 +0800

    feat: first import of chatbot schema

--- psql-schema.sql
+++ psql-schema.sql
@@ -18,3 +18,45 @@ begin
 end;
 $$
 ;
+
+CREATE TABLE users (
+  user_id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  email VARCHAR(255) UNIQUE, 
+  password VARCHAR(255),
+  created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE channels (
+  channel_id SERIAL PRIMARY KEY,
+  name VARCHAR(255) UNIQUE 
+);
+
+CREATE TABLE conversations (
+  conversation_id BIGSERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users(user_id),
+  channel_id INTEGER REFERENCES channels(channel_id),
+  context TEXT,
+  created_at TIMESTAMP DEFAULT NOW()
+);
+CREATE INDEX conversation_user_id_idx ON conversations(user_id);
+CREATE INDEX conversation_channel_id_idx ON conversations(channel_id);
+
+CREATE TABLE down_vote_reasons (
+  reason_id SERIAL PRIMARY KEY,
+  description VARCHAR(255) UNIQUE 
+);
+
+CREATE TABLE messages (
+  message_id BIGSERIAL PRIMARY KEY,
+  conversation_id INTEGER REFERENCES conversations(conversation_id),
+  user_id INTEGER REFERENCES users(user_id), 
+  user_message TEXT,
+  bot_message TEXT,
+  vote smallint, -- 0: neutral, 1: thumb_up, -1: thumb_down
+  down_vote_reason_id INTEGER REFERENCES down_vote_reasons(reason_id), 
+  user_feedback TEXT,
+  created_at TIMESTAMP DEFAULT NOW() 
+);
+CREATE INDEX message_conversation_id_idx ON messages(conversation_id); 
+CREATE INDEX message_user_id_idx ON messages(user_id);
