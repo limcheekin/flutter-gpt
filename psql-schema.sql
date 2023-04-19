0b3ead5102e4da30bb6aa900282295ea31e3d65c
Lim Chee Kin <limcheekin@vobject.com>
2023-04-19 16:57:51 +0800

    feat: removed table creation script as table created by langchain automatically and updated function to use langchain_pg_embedding table

--- psql-schema.sql
+++ psql-schema.sql
@@ -1,29 +1,18 @@
--- REF: https://github.com/langchain-ai/langchain-template-supabase/blob/main/supabase/migrations/20230311201746_setup-vector-store.sql
-create table documents (
-  id varchar(36) primary key,
-  name varchar(255) not null,
-  content text,
-  checksum varchar(128),
-  token_count integer, 
-  embedding vector(1536),  -- 1536 works for OpenAI embeddings, change if needed
-  metadata jsonb
-);
-
 -- Create a function to search for documents
-create function match_documents(query_embedding vector(1536), match_count int)
-returns table(id varchar(36), content text, metadata jsonb, token_count integer, similarity float)
+create function match_documents(embedding vector(768), match_count int)
+returns table(uuid uuid, document text, cmetadata json, custom_id text, similarity float)
 language plpgsql
 as $$
 #variable_conflict use_column
 begin
   return query
   select
-    id,
-    content,
-    metadata,
-    token_count,
+    uuid,
+    document,
+    cmetadata,
+    custom_id,
     1 - (documents.embedding <=> query_embedding) as similarity
-  from documents
+  from langchain_pg_embedding documents
   order by documents.embedding <=> query_embedding
   limit match_count;
 end;
