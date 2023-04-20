b6e08f3bef148e8159f783e4a1ceab6d58901004
Lim Chee Kin <limcheekin@vobject.com>
2023-04-20 13:01:54 +0800

    feat: first revised version of db schema

--- psql-schema.sql
+++ psql-schema.sql
@@ -19,43 +19,61 @@ end;
 $$
 ;
 
+DROP TABLE messages CASCADE;
+DROP TABLE down_vote_reasons CASCADE;
+DROP TABLE conversations CASCADE;
+DROP TABLE users CASCADE;
+DROP TABLE channels CASCADE;
+DROP TABLE models CASCADE;
+
 CREATE TABLE users (
-  user_id SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   name VARCHAR(255),
   email VARCHAR(255) UNIQUE, 
   password VARCHAR(255),
   created_at TIMESTAMP DEFAULT NOW()
 );
 
+CREATE TABLE models (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) UNIQUE,
+  max_tokens INTEGER,
+  cmetadata JSON 
+);
+
 CREATE TABLE channels (
-  channel_id SERIAL PRIMARY KEY,
-  name VARCHAR(255) UNIQUE 
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) UNIQUE,
+  cmetadata JSON 
 );
 
 CREATE TABLE conversations (
-  conversation_id BIGSERIAL PRIMARY KEY,
-  user_id INTEGER REFERENCES users(user_id),
-  channel_id INTEGER REFERENCES channels(channel_id),
+  id BIGSERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users(id),
+  channel_id INTEGER REFERENCES channels(id),
   context TEXT,
+  cmetadata JSON,
   created_at TIMESTAMP DEFAULT NOW()
 );
 CREATE INDEX conversation_user_id_idx ON conversations(user_id);
 CREATE INDEX conversation_channel_id_idx ON conversations(channel_id);
 
 CREATE TABLE down_vote_reasons (
-  reason_id SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   description VARCHAR(255) UNIQUE 
 );
 
 CREATE TABLE messages (
-  message_id BIGSERIAL PRIMARY KEY,
-  conversation_id INTEGER REFERENCES conversations(conversation_id),
-  user_id INTEGER REFERENCES users(user_id), 
+  id BIGSERIAL PRIMARY KEY,
+  conversation_id BIGINT REFERENCES conversations(id),
+  user_id INTEGER REFERENCES users(id), 
   user_message TEXT,
   bot_message TEXT,
-  vote smallint, -- 0: neutral, 1: thumb_up, -1: thumb_down
-  down_vote_reason_id INTEGER REFERENCES down_vote_reasons(reason_id), 
+  vote SMALLINT, -- 0: neutral, 1: thumb_up, -1: thumb_down
+  down_vote_reason_id INTEGER REFERENCES down_vote_reasons(id), 
   user_feedback TEXT,
+  model_id INTEGER REFERENCES models(id), 
+  cmetadata JSON,
   created_at TIMESTAMP DEFAULT NOW() 
 );
 CREATE INDEX message_conversation_id_idx ON messages(conversation_id); 
